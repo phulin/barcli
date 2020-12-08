63e60d734abc2ab5312e9114d7ec6ad330972698
Patrick Hulin <patrick.hulin@gmail.com>
2020-12-07 21:06:17 -0500

    Trim scrollback.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,8 +4,10 @@ import GcliDisplay from './GcliDisplay';
 import useInterval from '../hooks/useInterval';
 import GcliInput from './GcliInput';
 
+const LINE_COUNT_LIMIT = 300;
+
 const App = () => {
-  const [gcliContents, setGcliContents] = useState([] as { id: number; text: string }[]);
+  const [gcliContents, setGcliContents] = useState([] as { id: number; text: string; lineCount: number }[]);
   const displayRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -16,7 +18,18 @@ const App = () => {
       const response = await fetch(`/KoLmafia/messageUpdate?pwd=${pwd}`);
       const html = await response.text();
       if (html.trim().length > 0) {
-        setGcliContents(gcliContents => [...gcliContents, { id: new Date().getTime(), text: html }]);
+        setGcliContents(gcliContents => {
+          // Trim so we have at most LINE_COUNT_LIMIT lines.
+          const lineCount = (html.match(/<br ?\/?>/g) || []).length;
+          let totalLineCount = gcliContents.reduce((sum, chunk) => sum + chunk.lineCount, 0) + lineCount;
+          let keepIndex = 0;
+          for (; keepIndex < gcliContents.length; keepIndex++) {
+            if (totalLineCount <= LINE_COUNT_LIMIT) break;
+            totalLineCount -= gcliContents[keepIndex].lineCount;
+          }
+          console.assert(totalLineCount >= lineCount);
+          return [...gcliContents.slice(keepIndex), { id: new Date().getTime(), text: html, lineCount }];
+        });
         displayRef.current?.scrollTo(0, displayRef.current?.scrollHeight);
         if (inputRef.current !== null) inputRef.current.value = '';
       }
